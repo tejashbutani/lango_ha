16542296f058b527ac1cc2546fa813825f2ff431
tejashbutani <tejasbutani78@gmail.com>
2025-06-19 17:38:49 +0530

    Update home screen to use DrawingScreen instead of DrawingScreen2 for consistency in navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DrawingScreen2(),
+      home: DrawingScreen(),
     );
   }
 }
